2543310c5134fb7ad0bbded95ae1e10825b60388
Timur Gilmullin <tgilmullin@ptsecurity.com>
2017-07-17 18:33:13 +0300

    Print Version is added. More examples added to README.

--- exampleproject/Main.py
+++ exampleproject/Main.py
@@ -8,8 +8,11 @@ def Main():
     """
     This is Main method that do nosting.
     """
-    return "This is Main module for ExampleProject that do nosting.\nRead more about DevOpsHQ Community here: https://github.com/devopshq/ExampleProject"
+    return "This is Main module for ExampleProject that do nothing.\nRead more about DevOpsHQ Community here: https://github.com/devopshq/ExampleProject"
 
 
 if __name__ == "__main__":
     print(Main())
+
+    import pkg_resources  # part of standart setuptools
+    print("Version of ExampleProject is [ {} ]".format(pkg_resources.get_distribution('dohq-example-project').version))
